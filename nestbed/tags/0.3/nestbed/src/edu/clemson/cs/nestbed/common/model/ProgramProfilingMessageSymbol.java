73221a8082bb91932c247624abffc8d51ff5f62d
adalton <adalton@20fa7c6b-af1d-0410-9b15-c5a7570799ef>
2006-02-20 09:46:13 +0000

    Version 0.3

---
+++ nestbed/tags/0.3/nestbed/src/edu/clemson/cs/nestbed/common/model/ProgramProfilingMessageSymbol.java
@@ -0,0 +1,113 @@
+/*
+ * ProgramProfilingMessageSymbol.java
+ *
+ * Network Embedded Sensor Testbed (NESTBed)
+ *
+ * Copyright (C) 2006
+ * Dependable Systems Research Group
+ * Department of Computer Science
+ * Clemson University
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ *
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301, USA.
+ */
+package edu.clemson.cs.nestbed.common.model;
+
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+
+public class ProgramProfilingMessageSymbol implements Serializable {
+    private int    id;
+    private int    programMessageSymbolID;
+    private int    projectDeploymentConfigurationID;
+    private Date   timestamp;
+
+
+    public ProgramProfilingMessageSymbol(int  id,
+                                         int  pdcID,
+                                         int  programMessageSymbolID,
+                                         Date timestamp) {
+
+        this.id                               = id;
+        this.programMessageSymbolID           = programMessageSymbolID;
+        this.projectDeploymentConfigurationID = pdcID;
+        this.timestamp                        = timestamp;
+    }
+    
+    public int getID() {
+        return id;
+    }
+
+
+    public int getProgramMessageSymbolID() {
+        return programMessageSymbolID;
+    }
+
+
+    public int getProjectDeploymentConfigurationID() {
+        return projectDeploymentConfigurationID;
+    }
+
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+
+        buffer.append("id:                               ");
+        buffer.append(id).append("\n");
+        buffer.append("programMessageSymbolID:           ");
+        buffer.append(programMessageSymbolID).append("\n");
+        buffer.append("projectDeploymentConfigurationID: ");
+        buffer.append(projectDeploymentConfigurationID).append("\n");
+        buffer.append("timestamp:                        ");
+        buffer.append(timestamp).append("\n");
+
+        return buffer.toString();
+    }
+
+
+    public boolean equals(ProgramProfilingMessageSymbol pmt) {
+        return    (id                      == pmt.id)
+               && (programMessageSymbolID  == pmt.programMessageSymbolID)
+               && (projectDeploymentConfigurationID ==
+                                        pmt.projectDeploymentConfigurationID)
+               && (timestamp.equals(pmt.timestamp));
+    }
+
+
+    public boolean equals(Object o) {
+        boolean equal = false;
+
+        if (o instanceof ProgramProfilingMessageSymbol) {
+            equal = equals((ProgramProfilingMessageSymbol) o);
+        }
+
+        return equal;
+    }
+
+
+    public int hashCode() {
+        return id;
+    }
+}
---
+++ nestbed/tags/0.3/nestbed/src/edu/clemson/cs/nestbed/server/management/MoteManagerImpl.java
@@ -0,0 +1,84 @@
+/*
+ * MoteManagerImpl.java
+ *
+ * Network Embedded Sensor Testbed (NESTBed)
+ *
+ * Copyright (C) 2006
+ * Dependable Systems Research Group
+ * Department of Computer Science
+ * Clemson University
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ *
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301, USA.
+ */
+package edu.clemson.cs.nestbed.server.management;
+
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import edu.clemson.cs.nestbed.common.management.MoteManager;
+import edu.clemson.cs.nestbed.common.model.Mote;
+import edu.clemson.cs.nestbed.server.adaptation.AdaptationException;
+import edu.clemson.cs.nestbed.server.adaptation.AdapterFactory;
+import edu.clemson.cs.nestbed.server.adaptation.AdapterType;
+import edu.clemson.cs.nestbed.server.adaptation.MoteAdapter;
+
+
+public class MoteManagerImpl extends    UnicastRemoteObject
+                             implements MoteManager {
+
+    private final static Log log =
+                LogFactory.getLog(MoteManagerImpl.class);
+
+
+    private MoteAdapter        moteAdapter;
+    private Map<Integer, Mote> motes;
+
+
+    public MoteManagerImpl() throws RemoteException {
+        super();
+
+        try {
+            moteAdapter = AdapterFactory.createMoteAdapter(AdapterType.SQL);
+            motes       = moteAdapter.readMotes();
+
+            log.debug("Motes read:\n" + motes);
+        } catch (AdaptationException ex) {
+            log.error("AdaptationException:", ex);
+            throw new RemoteException("AdaptationException:", ex);
+        }
+    }
+
+
+    public synchronized Mote getMote(int moteID) throws RemoteException {
+        log.debug("getMote() called");
+
+        return motes.get(moteID);
+    }
+
+
+    public synchronized List<Mote> getMoteList() throws RemoteException {
+        return new ArrayList<Mote>(motes.values());
+    }
+}
