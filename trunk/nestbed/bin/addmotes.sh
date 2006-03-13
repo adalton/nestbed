015cf311ddca003a3e7fc5299be7191eb079fbf9
adalton <adalton@20fa7c6b-af1d-0410-9b15-c5a7570799ef>
2006-03-13 20:35:03 +0000

    Cleanup nestbed.sql for to produce a 'clean' install.  Update script to create the table 'nestbed' instead of 'tosbed.'  Update supporting scripts to use new database name.

--- trunk/nestbed/bin/addmotes.sh
+++ trunk/nestbed/bin/addmotes.sh
@@ -7,4 +7,4 @@ MOTES=$(ls /dev/motes)
 for i in ${MOTES}; do
     echo "INSERT INTO Motes(moteSerialID, moteTypeID) VALUES('$i', 3);"
 done
-) | mysql -u tosbed -p tosbed
+) | mysql -u nestbed -p nestbed
